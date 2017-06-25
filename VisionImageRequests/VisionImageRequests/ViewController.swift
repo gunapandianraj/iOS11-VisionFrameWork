5f64da391adcbf98d6bff7ad25ea3e6db1b55e72
Gunapandian <gunapandianraj@gmail.com>
2017-06-25 22:32:20 +0530

    Project setup

---
+++ VisionImageRequests/VisionImageRequests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VisionImageRequests
+//
+//  Created by Gunapandian on 25/06/17.
+//  Copyright © 2017 Gunapandian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
